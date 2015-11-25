0408a7734414fdc85964c50dac8f86efd66314d6
sgpark <ghdrptkd@naver.com>
2015-11-25 14:02:48 +0900

    Mapper 연결

---
+++ studyBbs_Boom_v1/src/main/java/com/study/boom/bbs/BbsItemServiceImpl.java
@@ -0,0 +1,20 @@
+package com.study.boom.bbs;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service("bbsItemService")
+public class BbsItemServiceImpl implements BbsItemService
+{
+	@Autowired
+	private BbsItemMapper bbsItemMapper;
+
+	@Override
+	public List<BbsItem> findItems(BbsItem item)
+	{
+		// TODO Auto-generated method stub
+		return bbsItemMapper.findItems(item);
+	}
+}
